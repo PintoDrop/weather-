38542cf8cf277f7d8ce30155cd696c9f1dc83c77
PintoDrop <mikebender22@gmail.com>
2023-03-07 19:00:35 -0800

    fixed some weather display issues and added style

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
-// var startDt = dayjs().add(1, "day").startOf("day").unix();
-// var endDt = dayjs().add(6, "day").startOf("day").unix();
-
 $(document).ready(function () {
   var today = $("#today");
   var fiveDaySection = $("#fiveDay");
+  var searchCity = $("#search-city");
 
   function weatherFunction(searchTerm) {
-    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-    //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
     // Current weather:
     var url_weather1 = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=e89986efe36f78ede4b3bc4f08baa878`;
@@ -21,8 +16,6 @@ $(document).ready(function () {
     fetch(url_coordinate)
       .then((res) => res.json())
       .then((data) => {
-        // let lat = res.data.city.coord.lat
-        // let lon = res.data.city.coord.lon
         console.log(data[0].lon);
         console.log(data[0].lat);
 
@@ -40,10 +33,6 @@ $(document).ready(function () {
             console.log(data);
             // renderToday(data);
             fiveDay(data);
-            // for (var i=0; i< data.list.length; i+=5) {
-            //   var currentDay = data.list[i]
-            // fiveDay(currentDay)
-            // }
           });
       });
   }
@@ -52,13 +41,12 @@ $(document).ready(function () {
     var searchTerm = $("#search-value").val();
     console.log(searchTerm);
     $("#search-value").val("");
-    // alert(searchTerm);
     weatherFunction(searchTerm);
-    // weatherForecast(searchTerm);
   });
 
   function renderToday(dataObject) {
-    var dateEl = $("<p>");
+    today.empty();
+    var dateEl = $("<h3>");
     var tempEl = $("<li>");
     var windEl = $("<li>");
     var weatherEl = $("<li>");
@@ -76,86 +64,75 @@ $(document).ready(function () {
     humidEl.text("Humidity: " + dataObject.main.humidity + " %");
     dateEl.text("Date: " + dayjs().format("MM/DD/YYYY"));
 
-    // iconEl.text(dataObject.list[0].weather.icon);
+    // searchCity
+    var newButton = $("<button>");
+    newButton.text(`${dataObject.name}`);
+
+    newButton.on("click", () => {
+      weatherFunction(dataObject.name);
+    });
+
+    searchCity.append(newButton);
 
     // order of appearance
     today.append(city, dateEl, iconEl, tempEl, windEl, weatherEl, humidEl);
   }
 
   function fiveDay(dataObject) {
-    // console.log(dataObject);
-    var dateEl = $("<p>");
-    var city = $("<h1>");
-    // var date = $("<h2>");
-    var tempEl = $("<li>");
-    var windEl = $("<li>");
-    var weatherEl = $("<li>");
-    var humidEl = $("<li>");
-    var iconEl = $("<img>");
-
-    var dateEl1 = $("<p>");
-    var humidEl1 = $("<p>");
+    fiveDaySection.empty();
 
     var startDate = dayjs().add(1, "day").startOf("day").unix();
     var endDate = dayjs().add(6, "day").startOf("day").unix();
     console.log(startDate, endDate);
     // change index'
-    var iconUrl = `https://openweathermap.org/img/w/${dataObject.list[1].weather[0].icon}.png`;
-    iconEl.attr("src", iconUrl);
-    
+
     for (var i = 0; i < dataObject.list.length; i++) {
       if (
         dataObject.list[i].dt >= startDate &&
         dataObject.list[i].dt < endDate
       ) {
         if (dataObject.list[i].dt_txt.slice(11, 13) == "12") {
-          // fiveDay(dataObject[i]);
           console.log(dataObject.list[i]);
-    // tempEl.text(dataObject.list[0].main.temp)
 
-    tempEl.text("Temperature: " + dataObject.list[i].main.temp + " Fahrenheit");
-    windEl.text("Wind Speed: " + dataObject.list[i].wind.speed + " MPH");
-    weatherEl.text("Weather: " + dataObject.list[i].weather[0].description);
-    humidEl.text("Humidity: " + dataObject.list[i].main.humidity + " %");
-    dateEl.text("Date: " + dataObject.list[i].dt_txt + "pm");
+          var tempEl = $("<li>");
+          var windEl = $("<li>");
+          var weatherEl = $("<li>");
+          var humidEl = $("<li>");
+          var iconEl = $("<img>");
+          var dateEl = $("<h3>");
+          var cityEl = $("<h1>");
 
-    
+          var iconUrl = `https://openweathermap.org/img/w/${dataObject.list[i].weather[0].icon}.png`;
+          iconEl.attr("src", iconUrl);
 
+          tempEl.text(
+            "Temperature: " + dataObject.list[i].main.temp + " Fahrenheit"
+          );
+          windEl.text("Wind Speed: " + dataObject.list[i].wind.speed + " MPH");
+          weatherEl.text(
+            "Weather: " + dataObject.list[i].weather[0].description
+          );
+          humidEl.text("Humidity: " + dataObject.list[i].main.humidity + " %");
+          dateEl.text("Date: " + dataObject.list[i].dt_txt + "pm");
+          console.log("hey");
 
-          // tempEl.text(
-          //   "Temperature: " + dataObject.list[6].main.temp + " Fahrenheit"
-          // );
-          // windEl.text("Wind Speed: " + dataObject.list[6].wind.speed + " MPH");
-          // weatherEl.text(
-          //   "Weather: " + dataObject.list[6].weather[0].description
-          // );
-          // humidEl1.text("Humidity: " + dataObject.list[i].main.humidity + " %");
-          // dateEl1.text("Date: " + dataObject[i].dt_txt + "pm");
+          var divEl = $("<div>");
+          divEl.addClass("each-day");
+
+          divEl.append(cityEl);
+          divEl.append(dateEl);
+          divEl.append(iconEl);
+          divEl.append(tempEl);
+          divEl.append(windEl);
+          divEl.append(weatherEl);
+          divEl.append(humidEl);
+
+          fiveDaySection.append(divEl);
+
+          
         }
       }
     }
 
-    city.text(dataObject.city.name);
-    fiveDaySection.append(
-      city,
-      dateEl,
-      iconEl,
-      tempEl,
-      windEl,
-      weatherEl,
-      humidEl
-    );
-    // fiveDay();
   }
 });
-
-// img/icon url:https://openweathermap.org/img/w/${forecast.weather[0].icon}.png
-
-// document.getElementById('search-button').addEventListener("click", event => {
-//   let searchValue = document.getElementById('searchValue').value
-//   console.log(searchTerm)
-// });
-
-// $("#9 .description").text(localStorage.getItem("9"));
-
-// url:"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={e89986efe36f78ede4b3bc4f08baa878}";
